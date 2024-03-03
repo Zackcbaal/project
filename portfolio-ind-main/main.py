bf81b1cad594f7cf15e1135b0b96564fd314a7e7
Zackcbaal <127035247+Zackcbaal@users.noreply.github.com>
2024-03-04 03:36:11 +0800

    update

--- portfolio-ind-main/main.py
+++ portfolio-ind-main/main.py
@@ -1,15 +1,28 @@
 #Импорт
-from flask import Flask, render_template,request, redirect
-
-
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-#Halaman Konten Berjalan
+# Fungsi untuk membuat tabel dalam database SQLite
+def create_table():
+    conn = sqlite3.connect('feedback.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS feedback 
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                 email TEXT, 
+                 comment TEXT)''')
+    conn.commit()
+    conn.close()
+
+# Panggil fungsi create_table() saat aplikasi dijalankan
+if __name__ == "__main__":
+    create_table()
+
+# Halaman Konten Berjalan
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 #Keterampilan Dinamis
 @app.route('/', methods=['POST'])
@@ -23,7 +36,21 @@ def process_form():
                            button_discord=button_discord,
                            button_html=button_html,
                            button_db=button_db,)
-
+    
+# Fungsi untuk menangani formulir umpan balik
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    if request.method == 'POST':
+        email = request.form['email']
+        comment = request.form['text']
+        # Simpan informasi ke dalam database SQLite
+        conn = sqlite3.connect('feedback.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO feedback (email, comment) VALUES (?, ?)", (email, comment))
+        conn.commit()
+        conn.close()
+        return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
